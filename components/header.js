ad2a81dd6796e1aa2fc37e449e0ed0dbb054beeb
Vikas Chauhan <vcikas2626@gmail.com>
2023-08-26 16:43:34 +0530

    Added Link to My Resume

--- components/header.js
+++ components/header.js
@@ -5,9 +5,19 @@ const Header = () => {
         VIKAS CHAUHAN
       </h1>
       <div className="flex justify-center items-center gap-10 mx-5 font-poppins text-darkslateblue font-normal md:hidden">
-        <a href="#projects" className="nav">Projects</a>
-        <a href="#contact" className="nav">Contact</a>
-        <a href="" className="nav">Resume</a>
+        <a href="#projects" className="nav">
+          Projects
+        </a>
+        <a href="#contact" className="nav">
+          Contact
+        </a>
+        <a
+          href="https://drive.google.com/file/d/1UcrpCaZagYpq3QJrMmkUOlt_rjZL-BoV/view?usp=sharing"
+          className="nav"
+          target="_blank"
+        >
+          Resume
+        </a>
       </div>
     </div>
   );
