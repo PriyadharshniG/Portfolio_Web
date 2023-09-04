4948e527da532329cc188105348e0b2da96ca5c1
Vikas Chauhan <vcikas2626@gmail.com>
2023-09-04 14:23:58 +0530

    Updated contact me component
    Added functionality to check first if fields are empty or not.

--- components/contact-me.js
+++ components/contact-me.js
@@ -2,7 +2,6 @@ import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import { motion } from "framer-motion";
 
-
 const variants = {
   hidden: { x: -100, opacity: 0 },
   visible: {
@@ -15,9 +14,26 @@ const variants = {
 const ContactMe = () => {
   const form = useRef();
   const [loading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  function isEmpty(name, email, message) {
+    name = name.trim();
+    email = email.trim();
+    message = message.trim();
+    if (name == "" || email == "" || message == "") {
+      return true;
+    }
+    return false;
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
+    if(isEmpty) {
+      return
+    }
+
     emailjs
       .sendForm(
         "service_hsy3aw3",
@@ -28,7 +44,7 @@ const ContactMe = () => {
       .then(
         (result) => {
           console.log(result.text);
-          setLoading(true)
+          setLoading(true);
           form.current.reset();
         },
         (error) => {
@@ -38,29 +54,43 @@ const ContactMe = () => {
   }
 
   return (
-    <motion.div className="w-full flex flex-col" variants={variants} initial='hidden' animate='visible' id="contact">
-      <motion.h1 className="font-poppins ml-8 md:ml-3 heading max-w-fit" id="contact">
+    <motion.div
+      className="w-full flex flex-col"
+      variants={variants}
+      initial="hidden"
+      animate="visible"
+      id="contact"
+    >
+      <motion.h1
+        className="font-poppins ml-8 md:ml-3 heading max-w-fit"
+        id="contact"
+      >
         Let's Connect
       </motion.h1>
-      <motion.p className="text-dimgray text-sm ml-8 md:ml-3 -mt-3 font-poppins overflow-hidden">
+      <motion.p className="text-dimgray text-lg font-medium ml-8 md:ml-3 -mt-3 font-poppins overflow-hidden">
         I would love to hear from you and discuss any potential opportunities or
         collaborations.
       </motion.p>
       <form
         ref={form}
         className="w-1/2 ml-8 md:ml-3 flex flex-row flex-wrap gap-6 md:w-[20rem]"
+        id="contact-form"
       >
         <input
           name="user_name"
           type="text"
           placeholder="Name"
           className="border-2 border-solid border-[rgb(161, 175, 194/1)] focus:border-darkslateblue rounded-2xl px-5 py-4 font-poppins text-lg outline-none transition-all md:w-64"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         <input
           name="user_email"
           type="text"
           placeholder="Email"
           className="border-2 border-solid border-[rgb(161, 175, 194/1)] focus:border-darkslateblue rounded-2xl px-5 py-4 font-poppins text-lg outline-none transition-all md:w-64"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <textarea
           name="message"
@@ -68,10 +98,12 @@ const ContactMe = () => {
           rows="10"
           placeholder="Message"
           className="w-full border-2 border-solid border-[rgb(161, 175, 194/1)] focus:border-darkslateblue rounded-2xl font-poppins pt-3 pl-3 text-lg outline-none transition-all"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         ></textarea>
         <motion.button
-          whileHover={{scale: 1.1}}
-          whileTap={{scale: 0.9}}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
           type="button"
           className="text-lg font-poppins rounded-3xl px-5 py-2 text-darkslateblue bg-white border-2 border-solid border-darkslateblue flex flex-row items-center"
           onClick={handleSubmit}
